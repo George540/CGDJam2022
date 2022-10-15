f91874615cbbff17fbd0c640b34f199f537405fa
Armando Russo <russo_449@hotmail.com>
2022-10-14 23:45:22 -0400

    Add box trap

---
+++ ProjectBooCat/Assets/Scripts/Game/BoxFallingScript.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BoxFallingScript : MonoBehaviour
+{
+
+    [SerializeField] private SpriteRenderer Box;
+    [SerializeField] private SpriteRenderer BoxCapsule;
+    [SerializeField] private float NumberOfBoxes;
+    [SerializeField] private float SpawnInterval;
+    private float currentTime = 0f;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerStay");
+        
+        currentTime += Time.deltaTime;
+        
+        if(currentTime >= SpawnInterval)
+            SpawnBox();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        CancelInvoke("SpawnBox");
+    }
+
+    private void SpawnBox()
+    {
+        Instantiate(Box, BoxCapsule.transform.position, Quaternion.identity);
+        currentTime = 0f;
+    }
+
+}
