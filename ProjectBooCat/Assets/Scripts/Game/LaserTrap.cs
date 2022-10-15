fa33b65ca2e6f5af639c8ccf1eff9c0ac7c8f3d9
Armando Russo <russo_449@hotmail.com>
2022-10-15 12:53:27 -0400

    Add hitbox to laser trap

--- ProjectBooCat/Assets/Scripts/Game/LaserTrap.cs
+++ ProjectBooCat/Assets/Scripts/Game/LaserTrap.cs
@@ -2,20 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class LaserTrap : MonoBehaviour
 {
     [SerializeField] private LineRenderer Laser;
+    [SerializeField] private EdgeCollider2D Hitbox;
+    private List<Vector2> Points;
+    private enum HitState
+    {
+        Hit, NotHit
+    }
     
+    private HitState CurrentState;
+
+    private void Awake()
+    {
+        Points = new List<Vector2>();
+        Points.Add(Laser.GetPosition(0));
+        CurrentState = HitState.NotHit;
+    }
+
     private void FixedUpdate()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
-
-        if (hit.collider != null)
+        if (CurrentState == HitState.NotHit)
         {
-            var hitPosition = hit.transform.position;
-            
-            Laser.SetPosition(1, hitPosition);
+            if (hit.collider != null)
+            {
+                var hitPosition = hit.transform.position;
+                CurrentState = HitState.Hit;
+                Laser.SetPosition(1, hitPosition);
+                Points.Add(Laser.GetPosition(1));
+                var allPoints = Points.ToArray();
+                Hitbox.points = allPoints;
+            }
         }
     }
 }
