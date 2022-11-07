941b7b462c000451d5c19eb843f6dbdc4fa12140
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-11-07 16:39:14 -0500

    Make bear trap retractable

--- ProjectBooCat/Assets/Scripts/Mechanics/DeathZone.cs
+++ ProjectBooCat/Assets/Scripts/Mechanics/DeathZone.cs
@@ -15,21 +15,42 @@ namespace Platformer.Mechanics
     {
         private GameManager _gameManager;
         [SerializeField] private Collider2D _collider2D;
+        [SerializeField] private Animator _animator;
+        public AudioSource _audioSource;
+        public AudioClip _bearTrap;
+        public AudioClip _bearTrapOpen;
+        public float _openTimer;
+        public bool _isClosed;
+        
 
         private void Start()
         {
             _gameManager = GameManager.Instance;
         }
 
-        void OnTriggerStay2D(Collider2D collider)
+        void OnTriggerEnter2D(Collider2D collider)
         {
             var p = collider.gameObject.GetComponent<PlayerController>();
-            if (p != null)
+            if (p != null && p._isAlive && p.controlEnabled && !_isClosed)
             {
-                GetComponent<AudioSource>().Play();
+                _animator.Play("TriggerTrap");
+                _audioSource.PlayOneShot(_bearTrap);
                 _gameManager.SwitchToGhostState();
-                _collider2D.enabled = false;
+                _isClosed = true;
+                StartCoroutine(OpenBearTrap());
             }
+        }
+
+        private IEnumerator OpenBearTrap()
+        {
+            yield return new WaitForSeconds(_openTimer);
+            _audioSource.PlayOneShot(_bearTrapOpen);
+            _animator.Play("TriggerOpen");
+        }
+
+        public void ActivateBearTrap()
+        {
+            _isClosed = false;
         }
     }
 }
