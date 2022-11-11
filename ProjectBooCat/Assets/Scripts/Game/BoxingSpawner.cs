3e9111ab91c6fe87d0ec9d41d2ee596fbc66d68e
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-11-11 13:01:57 -0500

    Reduce sprite size & lab fixes

--- ProjectBooCat/Assets/Scripts/Game/BoxingSpawner.cs
+++ ProjectBooCat/Assets/Scripts/Game/BoxingSpawner.cs
@@ -1,15 +1,24 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxingSpawner : MonoBehaviour
 {
+    public AudioSource _audioSource;
     public Transform _dropPlace;
     public GameObject _fakeCollider;
     public GameObject _dropItem;
+    public GameObject _bombObject;
     public GameObject _bomb;
     public bool _hasItemDropped;
-    public bool isWeightTrap;
+    public bool _isWeightTrap;
+
+    private void Start()
+    {
+        if (_isWeightTrap)
+        {
+            SpawnBomb();
+        }
+    }
 
     public void DropItem()
     {
@@ -20,17 +29,33 @@ public class BoxingSpawner : MonoBehaviour
             _fakeCollider.GetComponent<Collider2D>().enabled = false;
             _fakeCollider.SetActive(false);
         }
-        if (!isWeightTrap)
+        if (!_isWeightTrap)
         {
             var drop = Instantiate(_dropItem, _dropPlace.position, Quaternion.identity);
             drop.GetComponent<Rigidbody2D>().simulated = true;
         }
         else
         {
-            _bomb.transform.parent = null;
-            _bomb.GetComponent<Rigidbody2D>().simulated = true;
+            _bombObject.transform.parent = null;
+            _bombObject.GetComponent<Rigidbody2D>().simulated = true;
+            _bombObject = null;
             _hasItemDropped = true;
+            StartCoroutine(EquipNewWeight());
         }
+    }
+
+    private IEnumerator EquipNewWeight()
+    {
+        yield return new WaitForSeconds(10.0f);
+        SpawnBomb();
+        _audioSource.Play();
+    }
+
+    private void SpawnBomb()
+    {
+        _bombObject = Instantiate(_bomb, transform);
+        _bombObject.GetComponent<Rigidbody2D>().simulated = false;
+        _hasItemDropped = false;
     }
 
 }
